a027e88711b2acfc6ad0b0a2c8a7c236c780e4b6
Adii-x <a.deokar2007@gmail.com>
2026-05-21 16:58:16 +0530

    Truthy and Falsy Done

---
+++ Js-Practice-Questions/script9.js
@@ -0,0 +1,25 @@
+// ## Truthy & Falsy
+
+// 1. Check whether an empty string is truthy or falsy.
+// 2. Check whether `0` is truthy or falsy.
+// 3. Check whether `[]` is truthy or falsy.
+// 4. Create a variable and print `"Valid"` if it has a value otherwise print `"Invalid"`.
+
+// Questions 1 -
+
+console.log(!!"");
+
+// Question 2 -
+
+console.log(!!0);
+
+// Question 3 - 
+
+console.log(!![]);
+
+// Question 4 - 
+
+var age;
+
+if(age) console.log("Valid");
+else console.log("Invalid");
